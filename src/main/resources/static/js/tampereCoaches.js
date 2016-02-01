d443149475e4762125235bf466a1c38b945ee8ca
Mika Turunen <mika.turunen@ymail.com>
2016-02-01 15:18:53 +0200

    Update controller to match the SPA ngRoute use.

--- src/main/resources/static/js/tampereCoaches.js
+++ src/main/resources/static/js/tampereCoaches.js
@@ -1,7 +1,26 @@
 
-angular.module("tampereCoaches", [])
+var moduleName = "tampereCoaches";
+var dependencies = [
+	"ngRoute";
+]
+
+angular.module(moduleName, dependencies)
+	.config(function($routeProvider, $httpProvider) {
+		$routeProvider.when("/", {
+			templateUrl: "greetings.html",
+			controller: "home"
+		})
+		.when("/login", {
+			templateUrl: "login.html",
+			controller: "nagivation"
+		})
+		.otherwise("/");
+	})
 	.controller("home", function($scope, $http) {
 		$http.get("/resources").success(function(model) {
 			$scope.greeting = model;
 		});
+	})
+	.controller("navigation", function() {
+
 	});
