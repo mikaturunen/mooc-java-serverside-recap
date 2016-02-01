26c913e7b0d6f1496838a7c6423309563d592252
Mika Turunen <mika.turunen@ymail.com>
2016-02-01 15:38:37 +0200

    Support for fetching the user and using the custom CORS header with Angular.

--- src/main/java/com/application/UiApplication.java
+++ src/main/java/com/application/UiApplication.java
@@ -2,9 +2,18 @@ package com.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -19,6 +28,33 @@ public class UiApplication {
 		model.put("id", UUID.randomUUID().toString());
 		model.put("content", "Hello, World");
 		return model;
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.httpBasic()
+				.and()
+				.authorizeRequests()
+				.antMatchers("/ihdex.html", "/home.html", "login.html").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
+		}
+
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+			repository.setHeaderName("X-XRSF-TOKEN");
+			return repository;
+		}
 	}
 
 
