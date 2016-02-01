46d48947ed0b37eb677c5a8c6c56a2a9de3a810d
Mika Turunen <mika.turunen@ymail.com>
2016-02-01 15:24:01 +0200

    Create get functionality for the authenticated user.

--- src/main/resources/static/js/tampereCoaches.js
+++ src/main/resources/static/js/tampereCoaches.js
@@ -21,6 +21,34 @@ angular.module(moduleName, dependencies)
 			$scope.greeting = model;
 		});
 	})
-	.controller("navigation", function() {
+	.controller("navigation", function($tooScope, $scope, $http, $location) {
+		var authentication = function(credentials, callback) {
+			var headers = credentials ?
+				{ authorization: "Basic " + btoa(credentials.username + ":" + credentials.password) } :
+				{ };
 
+			$http.get("user", { headers: headers }).success(function(data) {
+				$rootScope.authenticated = data.user ? true : false;
+				callback && callback();
+			})
+			.error(function() {
+				$rootScope.authenticated = false;
+				callback && callback();
+			});
+		}
+
+		authenticate();
+		$scope.credentials = { };
+
+		$scope.login = function() {
+			authenticate($scope.credentials, function() {
+				if ($rootScope.authenticated) {
+					$location.path("/");
+					$scope.error = false;
+				} else {
+					$location.path("/login");
+					$scope.error = true;
+				}
+			});
+		};
 	});
